cb34f0196dc77ca086530454a59168dc40f04c94
pkun <181250068@smail.nju.edu.cn>
2021-03-30 21:54:47 +0800

    feat: delete controller test

--- business/src/test/java/pgd/irbl/business/Service/UserServiceTest.java
+++ business/src/test/java/pgd/irbl/business/Service/UserServiceTest.java
@@ -1,8 +1,59 @@
 package pgd.irbl.business.Service;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pgd.irbl.business.VO.LoginRegisterVO;
+
 /**
  * @Author: pkun
  * @CreateTime: 2021-03-30 18:34
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    LoginRegisterVO loginRegisterVO;
+
+    @Before
+    public void setUp(){
+        loginRegisterVO = new LoginRegisterVO();
+        loginRegisterVO.setUsername("develop");
+        loginRegisterVO.setPassword("1234567");
+    }
+
+    @Test
+    public void Test1Register(){
+        userService.register(loginRegisterVO);
+        Assert.assertTrue(userService.login(loginRegisterVO).getSuccess());
+    }
+
+    @Test
+    public void Test2Login(){
+        Assert.assertTrue(userService.login(loginRegisterVO).getSuccess());
+    }
+
+    @Test
+    public void Test3Login(){
+        loginRegisterVO.setPassword("lichun");
+        Assert.assertFalse(userService.login(loginRegisterVO).getSuccess());
+    }
+
+    @Test
+    public void Test4Login(){
+        loginRegisterVO.setUsername("develop1");
+        Assert.assertFalse(userService.login(loginRegisterVO).getSuccess());
+    }
+
 }
