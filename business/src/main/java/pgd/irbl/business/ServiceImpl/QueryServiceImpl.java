4d04bc72fdd961185140e7fbf9c218ad6a7620b8
Qin <181250117@smail.nju.edu.cn>
2021-04-06 20:32:33 +0800

    add: 添加 proto 文件，定义用于 grpc 的数据格式；添加 查询相关接口

--- business/src/main/java/pgd/irbl/business/ServiceImpl/QueryServiceImpl.java
+++ business/src/main/java/pgd/irbl/business/ServiceImpl/QueryServiceImpl.java
@@ -1,16 +1,26 @@
 package pgd.irbl.business.ServiceImpl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import pgd.irbl.business.Service.QueryService;
 import pgd.irbl.business.VO.ResponseVO;
 
 /**
- * @Author: pkun
- * @CreateTime: 2021-03-30 15:46
+ * @Author: qin
+ * @CreateTime: 2021-04-06 20:20
  */
+@Service
 public class QueryServiceImpl implements QueryService {
 
     @Override
-    public ResponseVO queryRegister() {
+    public ResponseVO queryRegister(MultipartFile bugReport, String commitID) {
+        //todo
+        return null;
+    }
+
+    @Override
+    public ResponseVO queryNotRegister(MultipartFile bugReport, MultipartFile sourceCode){
+
         return null;
     }
 }
