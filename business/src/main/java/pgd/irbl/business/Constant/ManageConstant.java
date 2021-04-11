9cb11462fb1c30f9e19a281531096ad07fd6df83
Qin <181250117@smail.nju.edu.cn>
2021-04-11 16:05:33 +0800

    feat: finish QueryServiceImpl

--- business/src/main/java/pgd/irbl/business/Constant/ManageConstant.java
+++ business/src/main/java/pgd/irbl/business/Constant/ManageConstant.java
@@ -14,4 +14,5 @@ public class ManageConstant {
     public static final String REGISTER_FAIL = "注册失败";
     public static final String MODIFY_SUCCESS = "更新成功";
     public static final String DELETE_SUCCESS = "删除成功";
+    public static final String QUERY_FAIL = "查询失败";
 }
--- business/src/main/java/pgd/irbl/business/ServiceImpl/QueryServiceImpl.java
+++ business/src/main/java/pgd/irbl/business/ServiceImpl/QueryServiceImpl.java
@@ -1,13 +1,20 @@
 package pgd.irbl.business.ServiceImpl;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import pgd.irbl.business.Service.QueryService;
 import pgd.irbl.business.Utils.MyFileUtil;
 import pgd.irbl.business.VO.ResponseVO;
+import pgd.irbl.business.grpcClient.CalcClient;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static pgd.irbl.business.Constant.ManageConstant.QUERY_FAIL;
 
 /**
  * @Author: qin
@@ -41,9 +48,33 @@ public class QueryServiceImpl implements QueryService {
         if(bugReportFullPath==null || codeFullPath==null){
             return ResponseVO.buildFailure(null);
         }
-        //todo call gRPC
 
+        //todo set gRPC server port
+        String target = "localhost:50051";
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+        List<FileScore> fileScoreList = null;
+        try {
+            CalcClient client = new CalcClient(channel);
+            fileScoreList = client.calc(bugReportFullPath, codeFullPath);
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
 
-        return null;
+//            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+            channel.shutdownNow();
+        }
+
+        if(fileScoreList!=null){
+            return ResponseVO.buildSuccess(fileScoreList);
+        }
+        return ResponseVO.buildFailure(QUERY_FAIL);
     }
 }
