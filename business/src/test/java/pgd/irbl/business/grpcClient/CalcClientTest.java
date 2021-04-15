843bbea6cf77850b685d77c09c81440bd20eae8f
Qin <181250117@smail.nju.edu.cn>
2021-04-15 19:28:45 +0800

    add: remove python

---
+++ business/src/test/java/pgd/irbl/business/grpcClient/CalcClientTest.java
@@ -0,0 +1,32 @@
+package pgd.irbl.business.grpcClient;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.Test;
+import pgd.irbl.business.serviceImpl.FileScore;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author qin
+ * @description CalcClientTest
+ * @date 2021-04-15
+ */
+public class CalcClientTest {
+
+    @Test
+    public void calc() {
+        String targetPreProcessor = "localhost:50051";
+        ManagedChannel channelPreProcessor = ManagedChannelBuilder.forTarget(targetPreProcessor)
+                .usePlaintext()
+                .build();
+        CalcClient calcClient = new CalcClient(channelPreProcessor);
+        List<FileScore> fileScores = calcClient.calc("C:\\Users\\10444\\Desktop\\se3\\algorithm-irbl\\data\\fuck.txt", "swt");
+//        for (int i = 0; i < fileScores.size(); i++) {
+//            System.out.println(fileScores.get(i).getFilePath() + "   " + fileScores.get(i).getScore());
+//        }
+
+    }
+}
