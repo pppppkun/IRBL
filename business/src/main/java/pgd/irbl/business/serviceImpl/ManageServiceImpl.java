44c4822bda497b5ec33688b54e873e6583f59ae0
pkun <181250068@smail.nju.edu.cn>
2021-05-29 20:50:30 +0800

    feat: add git operation

--- business/src/main/java/pgd/irbl/business/serviceImpl/ManageServiceImpl.java
+++ business/src/main/java/pgd/irbl/business/serviceImpl/ManageServiceImpl.java
@@ -11,7 +11,13 @@ import pgd.irbl.business.vo.ModifyRepoVO;
 import pgd.irbl.business.vo.RegisterRepoVO;
 import pgd.irbl.business.vo.ResponseVO;
 import pgd.irbl.business.enums.RepoState;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ProgressMonitor;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
 
@@ -74,8 +80,63 @@ public class ManageServiceImpl implements ManageService {
         else return ResponseVO.buildSuccess(DELETE_SUCCESS);
     }
 
+    private static final String REMOTE_URL = "https://github.com/github/testrepo.git";
+
     @Override
     public ResponseVO dealWebhook() {
+        try {
+            File localPath = File.createTempFile("TestGitRepository", "");
+            if (!localPath.delete()) {
+                throw new IOException("Could not delete temporary file " + localPath);
+            }
+            // then clone
+            log.info("Cloning from " + REMOTE_URL + " to " + localPath);
+            try {
+                Git result = Git.cloneRepository()
+                        .setURI(REMOTE_URL)
+                        .setDirectory(localPath)
+                        .setProgressMonitor(new SimpleProgressMonitor())
+                        .call();
+                // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+                log.info("Having repository: " + result.getRepository().getDirectory());
+            } catch (GitAPIException e) {
+            }
+
+            //todo
+
+
+            // clean up here to not keep using more and more disk-space for these samples
+            FileUtils.deleteDirectory(localPath);
+            return null;
+        } catch (IOException Exception) {
+        }
         return null;
+    }
+
+    private static class SimpleProgressMonitor implements ProgressMonitor {
+        @Override
+        public void start(int totalTasks) {
+            System.out.println("Starting work on " + totalTasks + " tasks");
+        }
+
+        @Override
+        public void beginTask(String title, int totalWork) {
+            System.out.println("Start " + title + ": " + totalWork);
+        }
+
+        @Override
+        public void update(int completed) {
+            System.out.print(completed + "-");
+        }
+
+        @Override
+        public void endTask() {
+            System.out.println("Done");
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
     }
 }
