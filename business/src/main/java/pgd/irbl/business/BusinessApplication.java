bec500c9e298c09a711ab12a257ad255f2d457aa
pkun <181250068@smail.nju.edu.cn>
2021-06-10 22:12:21 +0800

    fix: fix bug

--- business/src/main/java/pgd/irbl/business/BusinessApplication.java
+++ business/src/main/java/pgd/irbl/business/BusinessApplication.java
@@ -1,23 +1,13 @@
 package pgd.irbl.business;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.*;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pgd.irbl.business.po.RepoCommit;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class BusinessApplication {
 
-    public static void main(String[] args) throws IOException {
-//        SpringApplication.run(BusinessApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
     }
 }
