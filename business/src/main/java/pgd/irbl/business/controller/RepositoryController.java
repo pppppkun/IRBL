6188abdccfe01019632066173300d02d85dbc3ab
pkun <181250068@smail.nju.edu.cn>
2021-06-13 12:29:55 +0800

    fix: fix mapping bug

--- business/src/main/java/pgd/irbl/business/controller/RepositoryController.java
+++ business/src/main/java/pgd/irbl/business/controller/RepositoryController.java
@@ -3,6 +3,7 @@ package pgd.irbl.business.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pgd.irbl.business.service.ManageService;
 import pgd.irbl.business.vo.ResponseVO;
@@ -12,17 +13,18 @@ import pgd.irbl.business.vo.ResponseVO;
  * @CreateTime: 2021-06-12 21:22
  */
 @RestController
+@RequestMapping("/repository")
 public class RepositoryController {
 
     @Autowired
     ManageService manageService;
 
-    @PostMapping("/repository/list")
+    @PostMapping("/list")
     public ResponseVO getAllRepo(){
         return ResponseVO.buildSuccess(manageService.getAllSimpleRepo());
     }
 
-    @PostMapping("/repositoryu/{id}/commit/list")
+    @PostMapping("/{id}/commit/list")
     public ResponseVO getRepoCommit(@PathVariable("id") Long id){
         return manageService.getRepoCommit(id);
     }
