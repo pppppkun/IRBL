429d5fbba98d1bb3bf08133d555a9b2c307777cf
pkun <181250068@smail.nju.edu.cn>
2021-06-18 19:19:57 +0800

    fix: fix bug

--- business/src/main/java/pgd/irbl/business/utils/GitUtil.java
+++ business/src/main/java/pgd/irbl/business/utils/GitUtil.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,7 +27,7 @@ public class GitUtil {
 
     public void copyAndReset(String source, String destination, String commitId) {
         try {
-            Process process = Runtime.getRuntime().exec("./reset.sh " + REPO_DIRECTION + source + " " + commitId + " " + REPO_DIRECTION + destination);
+            Process process = Runtime.getRuntime().exec("./reset.sh " + REPO_DIRECTION + destination + " " + commitId + " " + REPO_DIRECTION + source);
             InputStream inputStream = process.getInputStream();
             process.waitFor();
             try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
@@ -39,17 +40,17 @@ public class GitUtil {
     }
 
     public void deleteRepo(String gitUrl) {
-        try{
+        try {
             Process process = Runtime.getRuntime().exec("rm -rf " + getRegisterRepoPath(gitUrl));
             process.waitFor();
             process.destroy();
-        }catch (IOException | InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public String showFileSpecificCommit(String gitUrl, String commitId, String filePath) {
-        try{
+        try {
             Process process = Runtime.getRuntime().exec("./getFile.sh " + getRegisterRepoPath(gitUrl) + " " + commitId + " " + filePath);
             InputStream inputStream = process.getInputStream();
             process.waitFor();
@@ -59,19 +60,19 @@ public class GitUtil {
             process.destroy();
             Path path = Paths.get(REPO_DIRECTION + "result.txt");
             return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-        }catch (IOException | InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public String getRegisterRepoPath(String gitUrl){
+    public String getRegisterRepoPath(String gitUrl) {
         String repoName = gitUrl.substring(gitUrl.lastIndexOf("/") + 1, gitUrl.lastIndexOf(".git"));
         return REPO_DIRECTION + repoName + gitUrl.hashCode();
     }
 
 
-    public String getRegisterRepoGitPath(String gitUrl){
+    public String getRegisterRepoGitPath(String gitUrl) {
         return getRegisterRepoPath(gitUrl) + "/.git";
     }
 
