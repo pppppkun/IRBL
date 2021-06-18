d64f18689fbe61473ce7f65b473ad64e522b94a2
Qin <181250117@smail.nju.edu.cn>
2021-06-18 09:39:33 +0800

    feat: add user-defined thread pool

--- business/src/main/java/pgd/irbl/business/config/ThreadPoolConfig.java
+++ business/src/main/java/pgd/irbl/business/config/ThreadPoolConfig.java
@@ -1,46 +1,25 @@
 package pgd.irbl.business.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
-import pgd.irbl.business.po.User;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * @author qin
- * @description 线程池配置
- * @date 2021-06-09
+ * @description 线程池配置,使用线程池进行流量控制
+ * @date 2021-06-18
  */
 @Configuration
 public class ThreadPoolConfig {
 
-//    @Bean(name = "threadPool")
-//    public ThreadPoolExecutorFactoryBean executorServiceFactory () {
-//        ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean ();
-//        factory.setThreadNamePrefix ( "query" );
-//        factory.setMaxPoolSize ( 3 );
-//        factory.setCorePoolSize ( 2 );
-//        factory.setQueueCapacity ( 10000 );
-//        return factory;
-//    }
+    private static int nThreads = 2;
+    private static int limitThreads = 1000000;
 
-//    @Bean
-//    public ExecutorService executorService() {
-//        ThreadPoolExecutorFactoryBean factory = new ThreadPoolExecutorFactoryBean ();
-//        factory.setThreadNamePrefix ( "query" );
-//        factory.setMaxPoolSize ( 3 );
-//        factory.setCorePoolSize ( 2 );
-//        factory.setQueueCapacity ( 10000 );
-//        return factory.getObject();
-//    }
     @Bean
     ExecutorService getExecutorService(){
-        return Executors.newFixedThreadPool(2);
+        return new ThreadPoolExecutor(nThreads, nThreads,
+                60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(limitThreads));
     }
 
 }
